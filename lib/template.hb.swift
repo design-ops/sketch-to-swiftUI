d7a3e196524752d86011e0f29fcc7da6c206ed7a
Kerr Marin Miller <kerr@kerrmarin.com>
2020-09-03 17:29:15 +0100

    Update so template uses UIKitStyleContainer

--- lib/template.hb.swift
+++ lib/template.hb.swift
@@ -29,22 +29,28 @@ extension Stylist {
         }
 {{/each}}
 {{/if}}
-
-{{#if uiKitProps}}
-    {{#each uiKitProps}}
-        {{#if properties.length}}
-        stylist.addProperty(identifier: "{{name}}") {
-            return [
-                {{#each properties}}
-                    {{{this}}}{{#if @last}}{{else}},{{/if}}
-                {{/each}}
-            ]
-        }
-        {{/if}}
-    {{/each}}
-{{/if}}
-
         return stylist
+    }
+}
+
+extension UIKitStyleContainer {
+    static func create(with stylist: Stylist) -> UIKitStyleContainer {
+        let container = UIKitStyleContainer(stylist: stylist)
+        {{#if uiKitProps}}
+            {{#each uiKitProps}}
+                {{#if properties.length}}
+                container.addProperty(identifier: "{{name}}") {
+                    return [
+                        {{#each properties}}
+                            {{{this}}}{{#if @last}}{{else}},{{/if}}
+                        {{/each}}
+                    ]
+                }
+                {{/if}}
+            {{/each}}
+        {{/if}}
+
+        return container
     }
 }
 //swiftlint:enable all
