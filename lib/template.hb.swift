1ee0202fca182fb271dfe41a582784bf3f080be4
Kerr Marin Miller <kerr@kerrmarin.com>
2021-06-10 09:59:36 +0100

    Remove anyview from generated file

--- lib/template.hb.swift
+++ lib/template.hb.swift
@@ -17,15 +17,14 @@ extension Stylist {
 {{#if styles}}
 {{#each styles}}
         stylist.addStyle(identifier: "{{name}}") {
-            AnyView($0{{#if text}}
-                      .styleText { text in
-                          text{{#each text}}
-                              .{{name}}({{{value}}}){{/each}}
-                      }{{/if}}
-                      {{#each style}}
-                        .{{name}}({{{value}}})
-                      {{/each}}
-            )
+            $0{{#if text}}
+                .styleText { text in
+                    text{{#each text}}
+                        .{{name}}({{{value}}}){{/each}}
+                }{{/if}}
+                {{#each style}}
+                .{{name}}({{{value}}})
+                {{/each}}
         }
 {{/each}}
 {{/if}}
